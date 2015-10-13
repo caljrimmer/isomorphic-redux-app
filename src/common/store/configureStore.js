3b7505d71b302cecfea72e8d51a20ae098b8cb31
Callum Rimmer <callum@deadtrendy.co.uk>
2015-10-13 17:52:25 +0100

    Added Logger Middleware

--- src/common/store/configureStore.js
+++ src/common/store/configureStore.js
@@ -2,9 +2,21 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { devTools } from 'redux-devtools';
 import { reduxReactRouter } from 'redux-router';
 import createHistory from 'history/lib/createBrowserHistory';
+import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
+let middleware = [];
+
+//Browser only middleware
+if(process.browser){
+  //Dev only middleare
+  if(process.env.NODE_ENV !== 'production'){
+    middleware = [...middleware,createLogger()]
+  }
+}
+
 const finalCreateStore = compose(
+  applyMiddleware(...middleware),
   devTools()
 )(createStore);
 
