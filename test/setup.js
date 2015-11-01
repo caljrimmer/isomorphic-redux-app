794c3bd8f1bd501325c7eca837243b3edd3bcbfb
Callum Rimmer <callum@deadtrendy.co.uk>
2015-11-01 09:48:19 +0000

    jsdom no contextify added to support node 4.0.0

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,4 @@
-var jsdom = require('jsdom');
+var jsdom = require('jsdom-no-contextify');
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.parentWindow;
